b5eddfaf2ad5ad7325da54bb6e8a9e64dd9159d3
Mirco874 <mircocuof@gmail.com>
2023-10-17 21:40:53 -0700

    first commit

---
+++ src/Report/service/Report.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Report } from "../schema/Report.schema";
+import { MongoRepository, Between } from "typeorm";
+import { CreateReportDto } from "../dto/CreateReport.dto";
+import mongoose from  "mongoose";
+
+@Injectable()
+export class ReportService {
+    constructor(
+        @InjectRepository(Report) private readonly reportRepository: MongoRepository<Report>
+    ){}
+
+    private getMongoId(mongoIdString): mongoose.Types.ObjectId{
+        return new mongoose.Types.ObjectId(mongoIdString);
+    }
+        
+
+    async findById(id: number): Promise<Report | null>{
+        try {
+            const findReport = this.reportRepository.findOneBy({_id: this.getMongoId(id)});
+            return findReport;
+        } catch (error) {
+            return null;
+        }
+    }
+
+    async findByUserId(userId: number): Promise<Report[]>{
+        return this.reportRepository.find({
+            userId,             
+            order: { date: "DESC"}
+        });
+    }
+
+    async test(userId: string): Promise<any>{
+        const something = this.reportRepository.find({
+            userId,
+            where: {
+                date: Between(new Date("21/09/2023"), new Date("22/09/2023"))
+            },
+            order: {
+                date: "DESC"
+            }
+        })
+        return something;
+    }
+
+
+
+
+    async persistReport( dto: CreateReportDto ): Promise<Report>{
+        const newReport = new Report();
+        newReport.cameraName = dto.cameraName;
+        newReport.inference = dto.inference;
+        newReport.message = dto.message;
+        newReport.date = new Date(dto.date);
+        newReport.time = dto.time;
+        newReport.userId = dto.userId;
+        newReport.imageURL = dto.imageURL;
+
+        return this.reportRepository.save(newReport);
+    } 
+
+    async deleteReportById(report: Report) {
+        this.reportRepository.remove(report);
+    }
+}
